ad8665a3ab32d6241e62f6765ecea74e840b8f05
sharyu raut <sharyukraut210298@gmail.com>
2020-11-17 00:26:40 +0530

    [Sharyu] Add. AddressBookSystem CURD operations and other queries

---
+++ AddressBookSQLQueries.sql
@@ -0,0 +1,28 @@
+create table AddressBook (
+	FirstName varchar(100),
+	LastName varchar(100),
+	Address varchar(100),
+	City varchar(100),
+	State varchar(100),
+	Zip varchar(10),
+	PhoneNo varchar(20),
+	Email varchar(100)
+);
+
+drop table AddressBook;
+
+select * from AddressBook;
+
+create table AddressBook2 (
+	FirstName varchar(100),
+	LastName varchar(100),
+	Address varchar(100),
+	City varchar(100),
+	State varchar(100),
+	Zip varchar(10),
+	PhoneNo varchar(20),
+	Email varchar(100)
+);
+
+select * from AddressBook;
+
